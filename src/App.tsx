404e466c40b40606e11cb57f962ea3c92d6610af
st-tuduong <tu.duong@supremetech.vn>
2022-07-12 15:38:22 +0700

    Fix UI SideBar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from './app/shared/components/layout';
+import './stylesheet/styles.scss';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
