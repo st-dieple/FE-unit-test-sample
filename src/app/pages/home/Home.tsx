88efb5725053cb92fa1123ebb349223d96ca41e4
st-longngo <long.ngo@supremetech.vn>
2022-07-13 10:53:44 +0700

    added UI banner

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Sidebar } from '../../shared/components/layout';
+import Banner from './partials/Banner';
 import SectionPost from './partials/SectionPost';
 
 const Home = () => {
   return (
     <main className="home-page">
-      <div className="container">
-        <div className="row">
-          <div className="col-8">
-            <SectionPost />
-          </div>
-          <div className="col-4">
-            <Sidebar />
+      <Banner />
+      <div className="content">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <SectionPost />
+            </div>
+            <div className="col-4">
+              <Sidebar />
+            </div>
           </div>
         </div>
       </div>
