72e5f776b8a31c117c759d9916318b8b1172f368
st-longngo <long.ngo@supremetech.vn>
2022-07-17 18:19:34 +0700

    fix validate dob and validate distance input and create UI error register

--- src/app/auth/childrens/Register.tsx
+++ src/app/auth/childrens/Register.tsx
@@ -11,23 +11,59 @@ import Loading from '../../shared/components/partials/Loading';
 const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }, getValues } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
 
-  const { data, hasError, isLoading, error } = useSelector((state: RootState) => state.register);
-  
+  const { data, hasError, isLoading, error } = useSelector(
+    (state: RootState) => state.register
+  );
+
   const onSubmit = (data: any) => {
-    dispatch(signUp({ data: {
-      ...data,
-      dob: data.dob.split('-').reverse().join('/')
-    }}));
+    dispatch(
+      signUp({
+        data: {
+          ...data,
+          dob: data.dob.split('-').reverse().join('/'),
+        },
+      })
+    );
   };
 
   useEffect(() => {
-    if(data) {
+    if (data) {
       navigate('/auth/login');
     }
     // eslint-disable-next-line
   }, [data]);
+
+  const validateDob = (time: string) => {
+    const regex = new RegExp(/^([0-9]{4})\-([0-9]{2})\-([0-9]{2})$/);
+
+    if (regex.test(time)) {
+      const parts = time.split('-');
+      const date = new Date();
+
+      if (date.getFullYear() < +parts[0]) {
+        return false;
+      } else {
+        if (date.getMonth() + 1 < +parts[1]) {
+          return false;
+        } else {
+          if (date.getDate() < +parts[2]) {
+            return false;
+          }
+        }
+      }
+    } else {
+      return false;
+    }
+
+    return true;
+  };
 
   return (
     <div className="form-auth row">
@@ -45,8 +81,11 @@ const Register = () => {
             name="firstName"
             placeholder="First Name"
             textLabel="First Name"
-            register={register("firstName", { required: true })}
-            isError={!errors.firstName}
+            register={register("firstName", {
+              required: true,
+              pattern: /^[a-zA-Z]+$/,
+            })}
+            isError={errors.firstName ? true : false}
             errorsMsg="First name is required."
           />
           <Input
@@ -54,8 +93,11 @@ const Register = () => {
             name="lastName"
             placeholder="Last Name"
             textLabel="Last Name"
-            register={register("lastName", { required: true })}
-            isError={!errors.lastName}
+            register={register("lastName", {
+              required: true,
+              pattern: /^[a-zA-Z]+$/,
+            })}
+            isError={errors.lastName ? true : false}
             errorsMsg="Last name is required."
           />
           <Input
@@ -63,9 +105,12 @@ const Register = () => {
             name="displayName"
             placeholder="User Name"
             textLabel="User Name"
-            register={register("displayName", { required: true })}
-            isError={!errors.displayName}
-            errorsMsg="Display name is required."
+            register={register("displayName", {
+              required: true,
+              pattern: /^[a-zA-Z0-9]+$/,
+            })}
+            isError={errors.displayName ? true : false}
+            errorsMsg="User name is required."
           />
           <Input
             type="text"
@@ -76,8 +121,10 @@ const Register = () => {
               required: true,
               pattern: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
             })}
-            isError={!errors.email}
-            errorsMsg={`Email address is ${getValues("email") ? "valid" : "required"}`}
+            isError={errors.email ? true : false}
+            errorsMsg={`Email address is ${
+              getValues("email") ? "valid." : "required."
+            }`}
           />
           <Input
             type="password"
@@ -89,7 +136,7 @@ const Register = () => {
               minLength: 8,
               maxLength: 20,
             })}
-            isError={!errors.password}
+            isError={errors.password ? true : false}
             errorsMsg="Please enter at least 8 characters."
           />
           <Input
@@ -97,9 +144,12 @@ const Register = () => {
             name="dob"
             placeholder="Date of Birth"
             textLabel="Date of Birth"
-            register={register("dob", { required: true })}
-            isError={!errors.dob}
-            errorsMsg="Date of birth is required."
+            register={register("dob", {
+              required: true,
+              validate: validateDob,
+            })}
+            isError={errors.dob ? true : false}
+            errorsMsg={`Date of birth is ${errors.dob && errors.dob.type}.`}
           />
           <div className="form-group">
             <select
@@ -112,12 +162,16 @@ const Register = () => {
             <label className="label">Gender</label>
           </div>
         </div>
-        {
-          hasError && <span className="txt-center txt-demi txt-error">{error.response.data.errors[0]}</span>
-        }
+        {hasError && (
+          <div className="error-box">
+            <span className="txt-center txt-error">
+              {error.response.data.errors[0]}
+            </span>
+          </div>
+        )}
         <div className="form-btn">
           <Button classBtn="btn btn-primary btn-auth" text="Sign up" />
-          {isLoading && <Loading classType="loading-small"/>}
+          {isLoading && <Loading classType="loading-small" />}
         </div>
         <p className="tip-text">
           Already have an account?
