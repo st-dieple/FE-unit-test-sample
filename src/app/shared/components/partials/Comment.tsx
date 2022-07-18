458abc10fd0695865f205701d494ac1800d73bc2
st-longngo <long.ngo@supremetech.vn>
2022-07-18 11:52:48 +0700

    remove div comment item

--- src/app/shared/components/partials/Comment.tsx
+++ src/app/shared/components/partials/Comment.tsx
@@ -4,35 +4,33 @@ import Image from '../../../../assets/images';
 
 const Comment = () => {
   return (
-    <li className="comment">
-      <div className="comment-item">
-        <div className="comment-header">
-          <Link to="/" className="comment-user">
-            <div className="user-avatar">
-              <img src={Image.Avatar} alt="avatar" />
-              <span className="user-active"></span>
-            </div>
-            <h4 className="user-name">Quoc Long</h4>
-          </Link>
-        </div>
-        <div className="comment-content">
-          <p className="comment-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
-            facere! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Nesciunt, rerum.
-          </p>
-        </div>
-        <div className="comment-footer">
-          <div className="comment-footer-item">
-            <i className="fa-regular fa-heart"></i>
-            <span>0</span>
+    <li className="comment-item">
+      <div className="comment-header">
+        <Link to="/" className="comment-user">
+          <div className="user-avatar">
+            <img src={Image.Avatar} alt="avatar" />
+            <span className="user-active"></span>
           </div>
-          <div className="comment-footer-item">
-            <i className="fa-regular fa-comment"></i>
-            <span>0</span>
-          </div>
-          <p className="comment-reply">Reply</p>
+          <h4 className="user-name">Quoc Long</h4>
+        </Link>
+      </div>
+      <div className="comment-content">
+        <p className="comment-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          facere! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nesciunt, rerum.
+        </p>
+      </div>
+      <div className="comment-footer">
+        <div className="comment-footer-item">
+          <i className="fa-regular fa-heart"></i>
+          <span>0</span>
         </div>
+        <div className="comment-footer-item">
+          <i className="fa-regular fa-comment"></i>
+          <span>0</span>
+        </div>
+        <p className="comment-reply">Reply</p>
       </div>
     </li>
   );
