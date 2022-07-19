3dc77fd0d6bc091b8e95b5d471c3763b6a43fe81
st-tuduong <tu.duong@supremetech.vn>
2022-07-19 11:33:49 +0700

    validate comment

--- src/app/pages/articles/partials/InteractComment.tsx
+++ src/app/pages/articles/partials/InteractComment.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Button, Input } from '../../../shared/components/partials';
+import { useForm } from 'react-hook-form';
+import { Button } from '../../../shared/components/partials';
 
 const InteractComment = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
-    <form className="section-comment">
-      <textarea className="txtArea form-control"></textarea>
+    <form className="section-comment" onSubmit={handleSubmit(onSubmit)}>
+      <div className="comment-content">
+        <textarea 
+          className="txtArea form-control"
+          placeholder="Write a comment"
+          {...register("txtArea", {
+            required: true,
+            maxLength: 800,
+          })}
+        />
+        {errors.txtArea && 
+          <span className="error-comment">Please type comment</span>
+        }
+      </div>
       <Button classBtn="btn btn-primary btn-comment" text="Comment"/>
     </form> 
    );
