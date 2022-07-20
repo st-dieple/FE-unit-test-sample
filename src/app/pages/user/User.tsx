59350f0eaaefa3bf4421456af87ce3e6aafd0582
st-tuduong <tu.duong@supremetech.vn>
2022-07-20 20:24:08 +0700

    Create My Articles UI

--- src/app/pages/user/User.tsx
+++ src/app/pages/user/User.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Image from "../../../assets/images";
-import { Button } from "../../shared/components/partials";
 import UserList from "./partials/UserList";
 
 const User = () => {
   return (
     <div className="container">
       <section className="section section-post">
-        <div className="author-info">
-          <div className="author-image">
+        <div className="author-info-content">
+          <div className="author-avatar">
             <Link to="/">
               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-BFO1-9_B8owATZcfnZc6FwA8GJjr-RMwHg&usqp=CAU" alt="image" />
             </Link>
           </div>
-          <div className="author-name">
-            <Link to="/">st-tuduong</Link>
-          </div>
-          <span></span>
-          <div className="author-follow">
-            <Button classBtn="btn btn-follow" text="Followers"/>
-            <Button classBtn="btn btn-follow" text="Following"/>
+          <div className="author-info">
+            <h2 className="author-name">ST - Tu Duong</h2>
+            <ul className="author-list">
+              <li className="author-item">5 Posts</li>
+              <li className="author-item">10 Followers</li>
+              <li className="author-item">15 Following</li>
+            </ul>
           </div>
         </div>
         <UserList />
