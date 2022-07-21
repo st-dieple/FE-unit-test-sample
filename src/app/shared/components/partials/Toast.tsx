fdbc8e0c6d2db1064482cf132b6f4e9e606afac0
st-longngo <long.ngo@supremetech.vn>
2022-07-21 08:47:20 +0700

    create toast component and added toast for create and update

---
+++ src/app/shared/components/partials/Toast.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Icon from "../../../../assets/icons";
+import { Button }  from './Button';
+
+interface IToastProp {
+  type: string,
+  title: string
+}
+
+export const Toast = ({type, title}: IToastProp) => {
+  let icon;
+  if(type === 'success') {
+    icon = Icon.Check;
+  }
+  if(type === 'error') {
+    icon = Icon.Error
+  }
+
+  return (
+    <div className={`toast toast-${type}`}>
+      <div className="toast-image">
+        <img src={icon} alt={type} />
+      </div>
+      <div>
+        <p className="toast-title">{title}</p>
+      </div>
+      <Button text={<i className="fa-solid fa-xmark"></i>} classBtn="toast-btn"/>
+    </div>
+  );
+};
+
+export default Toast;
--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -2,12 +2,16 @@ import Facebook from './facebook.svg';
 import Instagram from './instagram.svg';
 import Youtube from './youtube.svg';
 import Twitter from './twitter.svg';
+import Check from './check.svg';
+import Error from './error.svg';
 
 const Icon = {
   Facebook,
   Instagram,
   Youtube,
-  Twitter
+  Twitter,
+  Check,
+  Error
 };
 
 export default Icon;
