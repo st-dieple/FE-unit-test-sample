7b7060bca76014f8a961371783da346c7c78f828
st-tuduong <tu.duong@supremetech.vn>
2022-07-24 23:01:47 +0700

    Fix convention

--- src/app/core/serivces/article.service.ts
+++ src/app/core/serivces/article.service.ts
@@ -9,21 +9,21 @@ export class ArticleService {
 
   getPublicPosts(data: any) {
     return this.http.get([`${ENDPOINT.posts.public}?page=${data.page}&size=${data.size}`]);
-  }
+  };
 
   getPosts(data: any) {
     return this.http.get([`${ENDPOINT.posts.index}?page=${data.page}&size=${data.size}`]);
-  }
+  };
   
   createArticle(data: any) {
     return this.http.post([ENDPOINT.posts.index], data);
-  }
+  };
 
   updateArticle(id: string, data: any) {
     return this.http.put([`${ENDPOINT.posts.index}/${id}`], data);
-  }
+  };
 
   deleteArticle(id: string) {
     return this.http.delete([`${ENDPOINT.posts.index}/${id}`]);
-  }
-}
+  };
+};
