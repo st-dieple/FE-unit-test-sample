7fc3d08f66e108fd88a6d21a1a58e7006cbe3b91
st-tuduong <tu.duong@supremetech.vn>
2022-07-25 05:44:23 +0700

    Change scss icon like and comment

--- src/app/shared/components/partials/Post.tsx
+++ src/app/shared/components/partials/Post.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { deletePost } from '../../../pages/home/home.actions';
-import { Tag } from './Tag';
-import { formatDate } from './../../common/formatDate';
-import { checkUserId } from '../../common/checkUserId';
-import { IPost } from './../../interfaces/post';
-import Image from '../../../../assets/images';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../pages/home/home.actions";
+import { Tag } from "./Tag";
+import { formatDate } from "./../../common/formatDate";
+import { checkUserId } from "../../common/checkUserId";
+import { IPost } from "./../../interfaces/post";
+import Image from "../../../../assets/images";
 
 interface IPostProps {
   post: IPost;
@@ -30,8 +30,8 @@ export const Post = ({ post }: IPostProps) => {
                   src={post.user.picture || Image.Avatar}
                   alt={post.user.displayName}
                   onError={(e: any) => {
-                    e.target['onerror'] = null;
-                    e.target['src'] = Image.Avatar;
+                    e.target["onerror"] = null;
+                    e.target["src"] = Image.Avatar;
                   }}
                 />
               </div>
@@ -44,7 +44,10 @@ export const Post = ({ post }: IPostProps) => {
               <i className="fa-solid fa-ellipsis"></i>
               <ul className="post-control-list">
                 <li>
-                  <Link to={`/posts/${post.id}/edit`} className="post-control-item">
+                  <Link
+                    to={`/posts/${post.id}/edit`}
+                    className="post-control-item"
+                  >
                     <i className="fa-solid fa-pen"></i>
                     Edit
                   </Link>
@@ -70,33 +73,31 @@ export const Post = ({ post }: IPostProps) => {
               </Link>
             </h3>
             <p className="post-desc">{post.description}</p>
-            <div className="post-footer">
-              <div className="post-meta">
-                <div className="post-meta-info post-like">
-                  <i className="fa-regular fa-thumbs-up"></i>
-                  <span className="post-like-number">{post.likes || 0}</span>
-                </div>
-                <div className="post-meta-info post-comment">
-                  <i className="fa-regular fa-comment"></i>
-                  <span className="post-comment-number">
-                    {post.comments || 0}
-                  </span>
-                </div>
-              </div>
-              {post.tags && (
-                <ul className="post-tags">
-                  {post.tags.slice(-3).map((tag: any) => {
-                    return <Tag key={tag} name={tag} path="/" />;
-                  })}
-                </ul>
-              )}
-            </div>
           </div>
           <div className="post-image">
             <Link to={`/posts/${post.id}`} className="post-image-link">
               <img src={post.cover || Image.Empty} alt={post.title} />
             </Link>
           </div>
+        </div>
+        <div className="post-footer">
+          <div className="post-meta">
+            <div className="post-meta-info post-like">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <span className="post-like-number">{post.likes || 0}</span>
+            </div>
+            <div className="post-meta-info post-comment">
+              <i className="fa-regular fa-comment"></i>
+              <span className="post-comment-number">{post.comments || 0}</span>
+            </div>
+          </div>
+          {post.tags && (
+            <ul className="post-tags">
+              {post.tags.slice(-3).map((tag: any) => {
+                return <Tag key={tag} name={tag} path="/" />;
+              })}
+            </ul>
+          )}
         </div>
       </article>
     </li>
