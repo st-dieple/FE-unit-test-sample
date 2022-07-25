4c9ca532f402a97d487f4e822097a0c95285df02
st-tuduong <tu.duong@supremetech.vn>
2022-07-25 15:19:35 +0700

    Create UI Change Password

--- src/app/pages/user/user.routes.ts
+++ src/app/pages/user/user.routes.ts
@@ -1,10 +1,15 @@
 import { PageRoute } from '../../core/modules/custom-router-dom/router.interface';
+import UpdatePassword from './partials/UpdatePassword';
 import User from './User';
 
 const userRoutes: PageRoute[] = [
   {
     path: '/profile/:id',
     element: User
+  },
+  {
+    path: '/profile/change-password',
+    element: UpdatePassword
   },
 ];
 
