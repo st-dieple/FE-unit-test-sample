1d60d49537122c471b9f70e14f7190dfa6593252
st-longngo <long.ngo@supremetech.vn>
2022-07-25 13:49:15 +0700

    handle edit profile

--- src/app/auth/childrens/Register.tsx
+++ src/app/auth/childrens/Register.tsx
@@ -6,6 +6,7 @@ import { signUp } from './../auth.actions';
 import { RootState } from '../../app.reducers';
 import { Button, Input } from '../../shared/components/partials';
 import Image from '../../../assets/images';
+import { validateDob } from '../../shared/common/validateDob';
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -38,32 +39,6 @@ const Register = () => {
     }
     // eslint-disable-next-line
   }, [data]);
-
-  const validateDob = (time: string) => {
-    // eslint-disable-next-line
-    const regex = new RegExp(/^([0-9]{4})\-([0-9]{2})\-([0-9]{2})$/);
-    if (regex.test(time)) {
-      const parts = time.split('-');
-      const date = new Date();
-      
-      if (date.getFullYear() < +parts[0]) {
-        return 'invalid';
-      }
-      if(date.getFullYear() === +parts[0]){
-        if (date.getMonth() + 1 < +parts[1]) {
-          return 'invalid';
-        }
-        if(date.getMonth() + 1 === +parts[1]) {
-          if (date.getDate() < +parts[2]) {
-            return 'invalid';
-          }
-        }
-      }
-    } else {
-      return 'invalid';
-    }
-    return true;
-  }; 
 
   return (
     <div className="page-content row">
