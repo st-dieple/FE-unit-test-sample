1f765176e2c4c2f5dc85e1381af8b403b3d78dc3
st-longngo <long.ngo@supremetech.vn>
2022-07-26 11:49:42 +0700

    fix convention

--- src/app/core/serivces/article.service.ts
+++ src/app/core/serivces/article.service.ts
@@ -13,7 +13,7 @@ export class ArticleService {
         data.tags ? `tags=${data.tags}` : null
       }&page=${data.page}&size=${data.size}`,
     ]);
-  }
+  };
 
   getPosts(data: any) {
     return this.http.get([
@@ -21,17 +21,17 @@ export class ArticleService {
         data.page
       }&size=${data.size}`,
     ]);
-  }
+  };
 
   createArticle(data: any) {
     return this.http.post([ENDPOINT.posts.index], data);
-  }
+  };
 
   updateArticle(id: string, data: any) {
     return this.http.put([`${ENDPOINT.posts.index}/${id}`], data);
-  }
+  };
 
   deleteArticle(id: string) {
     return this.http.delete([`${ENDPOINT.posts.index}/${id}`]);
-  }
+  };
 }
