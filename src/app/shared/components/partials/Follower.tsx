cc475710beb3b09f015b119f8a8d10b7bc4a9e32
st-longngo <long.ngo@supremetech.vn>
2022-07-26 09:49:54 +0700

    get list followers, following and bind to view

--- src/app/shared/components/partials/Follower.tsx
+++ src/app/shared/components/partials/Follower.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Image from '../../../../assets/images';
 
-const Follower = () => {
+interface IFollowerProps {
+  follower: any;
+}
+
+const Follower = ({ follower }: IFollowerProps) => {
   return (
     <li className="follower">
       <div className="follower-item">
         <div className="follower-image">
-          <Link to='/' className="follower-image-link">
-            <img src={Image.Avatar} alt="" />
+          <Link to={`/profile/${follower?.id}`} className="follower-image-link">
+            <img
+              src={follower?.picture || Image.Avatar}
+              alt={follower?.displayName}
+              onError={(e: any) => {
+                e.target["onerror"] = null;
+                e.target["src"] = Image.Avatar;
+              }}
+            />
           </Link>
         </div>
         <div className="follower-info">
-          <Link to="/" className="follower-username">
-            <span>st-longngo</span>
+          <Link to={`/profile/${follower?.id}`} className="follower-username">
+            <span>{follower?.displayName}</span>
           </Link>
-          <p className="follower-name">Quoc Long</p>
+          <p className="follower-name">
+            {follower?.firstName} {follower?.lastName}
+          </p>
         </div>
       </div>
     </li>
