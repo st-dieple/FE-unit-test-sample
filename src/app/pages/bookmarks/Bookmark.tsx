64f63147b27191bbaf92fae6e2fd41031da453b0
st-tuduong <tu.duong@supremetech.vn>
2022-08-10 15:11:20 +0700

    get data bookmark and bind to view

--- src/app/pages/bookmarks/Bookmark.tsx
+++ src/app/pages/bookmarks/Bookmark.tsx
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { BookmarkService } from '../../core/serivces/bookmark.service';
+import SekeletonPost from '../../shared/components/partials/SekeletonPost';
 import PostList from '../posts/partials/PostList';
 
+const bookmarkService = new BookmarkService();
 const Bookmark = () => {
   const [posts, setPosts] = useState([]);
+  const [isRequestingAPI, setIsRequestingAPI] = useState<boolean>(false);
+
+  const getBookmark = () => {
+    if (!isRequestingAPI) {
+      setIsRequestingAPI(true);
+      bookmarkService
+        .getBookmark()
+        .then((res: any) => {
+          setIsRequestingAPI(false);
+          const newPost = res.map((item) => item.post);
+          setPosts(newPost);
+        })
+        .catch((error) => {
+          setIsRequestingAPI(false);
+        });
+    }
+  };
+
+  useEffect(() => {
+    getBookmark();
+  }, []);
 
   return (
     <section className="section-bookmark">
       <h2 className="section-title txt-center">My Bookmarks</h2>
-      <PostList posts={posts} />
+      {isRequestingAPI ? <SekeletonPost /> : <PostList posts={posts} />}
     </section>
   );
 };
