455ee64b227f1654491cd1c77fcf27c9775032ae
st-tuduong <tu.duong@supremetech.vn>
2022-08-10 17:17:45 +0700

    Change element

--- src/app/pages/posts/childrens/RecycleBin.tsx
+++ src/app/pages/posts/childrens/RecycleBin.tsx
@@ -5,10 +5,10 @@ const RecycleBin = () => {
   const [posts, setPosts] = useState([]);
 
   return (
-    <section className="section-content">
+    <main className="main-content">
       <h2 className="section-title txt-center">My Recycle Bin</h2>
       <PostList posts={posts} />
-    </section>
+    </main>
   );
 };
 
