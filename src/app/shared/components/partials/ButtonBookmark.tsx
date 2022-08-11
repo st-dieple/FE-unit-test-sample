96041e3772c6ad3be078ec4f65abe52c0462a534
st-longngo <long.ngo@supremetech.vn>
2022-08-11 15:53:55 +0700

    fix handle bookmark in my bookmark

--- src/app/shared/components/partials/ButtonBookmark.tsx
+++ src/app/shared/components/partials/ButtonBookmark.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useToast } from '../../contexts/toast.contexts';
 import { BookmarkService } from '../../../core/serivces/bookmark.service';
 import { Button } from './Button';
 import withAuthChecking from '../hoc/withAuthChecking';
 
 const bookmarkService = new BookmarkService();
 const ButtonBookmark = ({ post, checkAuthBeforeAction }: any) => {
+  const location = useLocation();
+  const toast = useToast();
   const [isInBookmark, setIsInBookmark] = useState<boolean>(post.isInBookmark);
   const [isRequestingAPI, setIsRequestingAPI] = useState<boolean>(false);
+
+  useEffect(() => {
+    if(location.pathname.includes('/bookmarks')) {
+      setIsInBookmark(true);
+    }
+  }, [location]);
 
   const addBookmark = () => {
     if (!isRequestingAPI) {
@@ -14,8 +24,19 @@ const ButtonBookmark = ({ post, checkAuthBeforeAction }: any) => {
       bookmarkService
         .addBookmark(post.id)
         .then((res: any) => {
-          setIsRequestingAPI(false);
           setIsInBookmark(res.isInBookmark);
+          if(res.isInBookmark) {
+            toast?.addToast({
+              type: 'success',
+              title: 'Add bookmark successfully.',
+            });
+          } else {
+            toast?.addToast({
+              type: 'success',
+              title: 'Remove bookmark successfully.',
+            });
+          }
+          setIsRequestingAPI(false);
         })
         .catch((err: any) => {
           setIsRequestingAPI(false);
